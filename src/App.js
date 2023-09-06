58ebc475ca9ac2b6cbd777d02e4eca780d8dafbf
Merliny <pontakorn_oom@hotmail.co.th>
2023-09-06 22:33:22 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-import Examplepage from './Page/ExamplePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from './pages';
+import About from './pages/About';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
 
-
-const App = () => {
-  return(
-    <div className = "App font-noto">
-      <Examplepage/>
-    </div>
-  )
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
