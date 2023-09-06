6512cc2ed61bcccb874f4d02ed526cafb06f7a2d
Merliny <pontakorn_oom@hotmail.co.th>
2023-09-06 21:57:27 +0700

    Test

---
+++ src/Page/ExamplePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MyPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to My React Page</h1>
+        </header>
+        <main>
+          <p>This is some content for my React page. You can add more elements and customize it as you like.</p>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default MyPage;
