1817e6cc586b2fc2257d6930a05270f4394a4b26
Merliny <pontakorn_oom@hotmail.co.th>
2023-10-13 18:50:37 +0700

    1
    s

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Slide from "./Components/Slide/slide";
-import Home from './pages';
-import About from './pages/Abouts';
-import Contact from './pages/contact';
 import SignUp from './pages/signup';
 import SignIn from './pages/signin';
+import OrderDetail from './pages/OrderDetail';
+import profile from './pages/Profile';
+import CreateSellOrder from './pages/CreateSellOrder';
+import BoosterForm from './pages/booster';
+
 
 function App() {
   return (
@@ -14,10 +16,12 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Slide />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/OrderDetail" element={<OrderDetail />} />
+        <Route path="/CreateSellOrder" element={<CreateSellOrder />} />
+        <Route path="/BoosterForm" element={<BoosterForm />} />
+        <Route path="/Profile" element={profile ()} />
       </Routes>
     </BrowserRouter>
   );
